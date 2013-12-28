415870a21362a3207b0fc73dcb9b12eb958930f5
Eric <ungericw@gmail.com>
2013-12-27 23:52:35 -0600

    color buffer function

--- iconGL.cpp
+++ iconGL.cpp
@@ -18,51 +18,99 @@ using namespace glm;
 #include "common/loadshader.hpp"
 #include "common/loadbmp.hpp"
 
-void CubeGeometry(GLfloat* vertex_buffer, GLfloat width){
+void CubeGeometry(GLfloat* vertex_buffer, GLfloat width, GLfloat x, GLfloat y, GLfloat z){
     GLfloat copy_vertex_buffer[36*3] = {
-        -width,-width,-width, // triangle 1 : begin
-        -width,-width, width,
-        -width, width, width, // triangle 1 : end
-        width, width,-width, // triangle 2 : begin
-        -width,-width,-width,
-        -width, width,-width, // triangle 2 : end
+        -width+x,-width+y,-width+z, // triangle 1 : begin
+        -width+x,-width+y, width+z,
+        -width+x, width+y, width+z, // triangle 1 : end
+        width+x, width+y,-width+z, // triangle 2 : begin
+        -width+x,-width+y,-width+z,
+        -width+x, width+y,-width+z, // triangle 2 : end
 
-        width,-width, width,
-        -width,-width,-width,
-        width,-width,-width,
-        width, width,-width,
-        width,-width,-width,
-        -width,-width,-width,
-        -width,-width,-width,
-        -width, width, width,
-        -width, width,-width,
-        width,-width, width,
+        width+x,-width+y, width+z,
+        -width+x,-width+y,-width+z,
+        width+x,-width+y,-width+z,
+        width+x, width+y,-width+z,
+        width+x,-width+y,-width+z,
+        -width+x,-width+y,-width+z,
+        -width+x,-width+y,-width+z,
+        -width+x, width+y, width+z,
+        -width+x, width+y,-width+z,
+        width+x,-width+y, width+z,
 
-        -width,-width, width,
-        -width,-width,-width,
-        -width, width, width,
-        -width,-width, width,
-        width,-width, width,
-        width, width, width,
-        width,-width,-width,
-        width, width,-width,
-        width,-width,-width,
-        width, width, width,
+        -width+x,-width+y, width+z,
+        -width+x,-width+y,-width+z,
+        -width+x, width+y, width+z,
+        -width+x,-width+y, width+z,
+        width+x,-width+y, width+z,
+        width+x, width+y, width+z,
+        width+x,-width+y,-width+z,
+        width+x, width+y,-width+z,
+        width+x,-width+y,-width+z,
+        width+x, width+y, width+z,
 
-        width,-width, width,
-        width, width, width,
-        width, width,-width,
-        -width, width,-width,
-        width, width, width,
-        -width, width,-width,
-        -width, width, width,
-        width, width, width,
-        -width, width, width,
-        width,-width, width
+        width+x,-width+y, width+z,
+        width+x, width+y, width+z,
+        width+x, width+y,-width+z,
+        -width+x, width+y,-width+z,
+        width+x, width+y, width+z,
+        -width+x, width+y,-width+z,
+        -width+x, width+y, width+z,
+        width+x, width+y, width+z,
+        -width+x, width+y, width+z,
+        width+x,-width+y, width+z
     };
 
     for (int i=0; i < 36*3; i++){
         vertex_buffer[i] = copy_vertex_buffer[i]; 
+    }
+
+}
+
+void CubeColor(GLfloat* color_buffer, GLfloat color){
+/*
+    GLfloat copy_color_buffer[36*3] = {
+        color,  color,  color,
+        color,  color,  color,
+        color,  color,  color,
+        color,  color,  color,
+        color,  color,  color,
+        color,  color,  0.185f,
+        0.597f,  0.770f,  0.761f,
+        0.559f,  0.436f,  0.730f,
+        0.359f,  0.583f,  0.152f,
+        0.483f,  0.596f,  0.789f,
+        0.559f,  0.861f,  0.639f,
+        0.195f,  0.548f,  0.859f,
+        0.014f,  0.184f,  0.576f,
+        0.771f,  0.328f,  0.970f,
+        0.406f,  0.615f,  0.116f,
+        0.676f,  0.977f,  0.133f,
+        0.971f,  0.572f,  0.833f,
+        0.140f,  0.616f,  0.489f,
+        0.997f,  0.513f,  0.064f,
+        0.945f,  0.719f,  0.592f,
+        0.543f,  0.021f,  0.978f,
+        0.279f,  0.317f,  0.505f,
+        0.167f,  0.620f,  0.077f,
+        0.347f,  0.857f,  0.137f,
+        0.055f,  0.953f,  0.042f,
+        0.714f,  0.505f,  0.345f,
+        0.783f,  0.290f,  0.734f,
+        0.722f,  0.645f,  0.174f,
+        0.302f,  0.455f,  0.848f,
+        0.225f,  0.587f,  0.040f,
+        0.517f,  0.713f,  0.338f,
+        0.053f,  0.959f,  0.120f,
+        0.393f,  0.621f,  0.362f,
+        0.673f,  0.211f,  0.457f,
+        0.820f,  0.883f,  0.371f,
+        0.982f,  0.099f,  0.879f
+    };   
+    */
+
+    for (int i=0; i<36*3; i++){
+        color_buffer[i] = color;
     }
 
 }
@@ -110,52 +158,10 @@ extern "C" int initGL(value argv[])
     glGenVertexArrays(1, &VertexArrayID);
     glBindVertexArray(VertexArrayID);
 
-
-    // Our vertices. Tree consecutive floats give a 3D vertex; 
-    // Three consecutive vertices give a triangle.
-    // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
-    /*
-    static const GLfloat g_vertex_buffer_data[] = {
-        -width,-width,-width, // triangle 1 : begin
-        -width,-width, width,
-        -width, width, width, // triangle 1 : end
-        width, width,-width, // triangle 2 : begin
-        -width,-width,-width,
-        -width, width,-width, // triangle 2 : end
-        width,-width, width,
-        -width,-width,-width,
-        width,-width,-width,
-        width, width,-width,
-        width,-width,-width,
-        -width,-width,-width,
-        -width,-width,-width,
-        -width, width, width,
-        -width, width,-width,
-        width,-width, width,
-        -width,-width, width,
-        -width,-width,-width,
-        -width, width, width,
-        -width,-width, width,
-        width,-width, width,
-        width, width, width,
-        width,-width,-width,
-        width, width,-width,
-        width,-width,-width,
-        width, width, width,
-        width,-width, width,
-        width, width, width,
-        width, width,-width,
-        -width, width,-width,
-        width, width, width,
-        -width, width,-width,
-        -width, width, width,
-        width, width, width,
-        -width, width, width,
-        width,-width, width
-    };*/
+    // Generate a cube geometry
     GLfloat width = 1.0f;
     GLfloat g_vertex_buffer_data[36*3];
-    CubeGeometry(g_vertex_buffer_data, width);
+    CubeGeometry(g_vertex_buffer_data, width,0, 0, 0);
 
     GLuint vertexbuffer;
     glGenBuffers(1, &vertexbuffer);
@@ -163,47 +169,10 @@ extern "C" int initGL(value argv[])
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
 
-
-    // One color for each vertex. They were generated randomly.
-    static const GLfloat g_color_buffer_data[] = {
-        0.583f,  0.771f,  0.014f,
-        0.609f,  0.115f,  0.436f,
-        0.327f,  0.483f,  0.844f,
-        0.822f,  0.569f,  0.201f,
-        0.435f,  0.602f,  0.223f,
-        0.310f,  0.747f,  0.185f,
-        0.597f,  0.770f,  0.761f,
-        0.559f,  0.436f,  0.730f,
-        0.359f,  0.583f,  0.152f,
-        0.483f,  0.596f,  0.789f,
-        0.559f,  0.861f,  0.639f,
-        0.195f,  0.548f,  0.859f,
-        0.014f,  0.184f,  0.576f,
-        0.771f,  0.328f,  0.970f,
-        0.406f,  0.615f,  0.116f,
-        0.676f,  0.977f,  0.133f,
-        0.971f,  0.572f,  0.833f,
-        0.140f,  0.616f,  0.489f,
-        0.997f,  0.513f,  0.064f,
-        0.945f,  0.719f,  0.592f,
-        0.543f,  0.021f,  0.978f,
-        0.279f,  0.317f,  0.505f,
-        0.167f,  0.620f,  0.077f,
-        0.347f,  0.857f,  0.137f,
-        0.055f,  0.953f,  0.042f,
-        0.714f,  0.505f,  0.345f,
-        0.783f,  0.290f,  0.734f,
-        0.722f,  0.645f,  0.174f,
-        0.302f,  0.455f,  0.848f,
-        0.225f,  0.587f,  0.040f,
-        0.517f,  0.713f,  0.338f,
-        0.053f,  0.959f,  0.120f,
-        0.393f,  0.621f,  0.362f,
-        0.673f,  0.211f,  0.457f,
-        0.820f,  0.883f,  0.371f,
-        0.982f,  0.099f,  0.879f
-    };
-
+    // Generate a material
+    GLfloat g_color_buffer_data[36*3];
+    CubeColor(g_color_buffer_data, 0.5f);
+    
     GLuint colorbuffer;
     glGenBuffers(1, &colorbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
